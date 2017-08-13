8e3fb057429575099f67bebb426e28736a86ba71
Ana Samoila <ana.samoila92@gmail.com>
2017-08-13 18:12:56 +0300

    Added index, css and js files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+	<meta charset="utf-8" />
+	<title>Github users finder</title>
+	<meta name="title" content="Github users finder" />
+	<meta name="description" content="Find github users based on a query" />
+	<meta name="author" content="Ana Samoila" />
+
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	
+</head>
+
+<body>
+
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+</body>
+</html>
