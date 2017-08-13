c6af1ac312d13e08f6ad93ad51b78fecfec1923e
Ana Samoila <ana.samoila92@gmail.com>
2017-08-13 19:46:01 +0300

    completed the SPA that fetches users info
    use the Github API to fetch users information

--- index.php
+++ index.php
@@ -6,14 +6,26 @@
 	<title>Github users finder</title>
 	<meta name="title" content="Github users finder" />
 	<meta name="description" content="Find github users based on a query" />
-	<meta name="author" content="Ana Samoila" />
 
+	<link href="https://bootswatch.com/simplex/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
 	
 </head>
 
 <body>
+	<div class="container">
+	    <div class="search-container">
+		<h1>Search Github users</h1>
+		<p class="lead">Enter a username to fetch a user's profile info and repos</p>
+		<input type="text" id="searchUser" class="form-control" placeholder="Github Username..."/>
+	    </div>
+	</div>
+	<br/>
+	<div id="profile"></div>
 
 	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
 </body>
 </html>
